88f1ce67693603fddce8e51625cc8152ca5f1612
Lizzie <lizzielizzietwoshoes@gmail.com>
2026-04-24 16:00:46 -0400

    Add Setting for Goal

--- scripts/logic/logic_helper.lua
+++ scripts/logic/logic_helper.lua
@@ -122,22 +122,6 @@ function key_check(keyitem,keylocation)
     return ANY(keyitem,ALL(Tracker:FindObjectForCode(keylocation).AccessibilityLevel,"vanilla"))
 end
 
-function count_music_boxes(number)
-    count = 0
-    if HAS("yellowmusicbox")==ACCESS_NORMAL then
-        count = count + 1
-    end
-    if HAS("bluemusicbox")==ACCESS_NORMAL then
-        count = count + 1
-    end
-    if HAS("greenmusicbox")==ACCESS_NORMAL then
-        count = count + 1
-    end
-    if HAS("redmusicbox")==ACCESS_NORMAL then
-        count = count + 1
-    end
-    if HAS("goldmusicbox")==ACCESS_NORMAL then
-        count = count + 1
-    end
-    return bool_to_accesslvl[count >= number]
+function count_music_boxes()
+    return Tracker:ProviderCountForCode("musicbox") >= Tracker:FindObjectForCode("goalsetting").CurrentStage
 end
